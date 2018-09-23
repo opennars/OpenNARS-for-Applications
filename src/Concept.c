4e3a01d8ae06d1eca327de2f050c638c2c490815
Patrick Hammer <patham9@gmail.com>
2018-09-23 15:09:04 +0100

    Concepts calculate their SDR when created, as will be required by the voting mechanism

--- src/Concept.c
+++ src/Concept.c
@@ -3,13 +3,13 @@
 void Concept_RESET(Concept *concept, SDR name)
 {
     concept->name = name;
-	concept->event_beliefs_amount = 0;
-	concept->precondition_beliefs_amount = 0;
-	concept->postcondition_beliefs_amount = 0;
-	//Generate CRC checksum too:
-	uint64_t crc = 0; 
-	ITERATE_SDR_BLOCKS(i,
-		crc = crc64(crc, &(name.blocks[i]), sizeof(SDR_BLOCK_TYPE));
-	)
-	concept->name_hash = crc;
+    concept->event_beliefs_amount = 0;
+    concept->precondition_beliefs_amount = 0;
+    concept->postcondition_beliefs_amount = 0;
+    //Generate CRC checksum too:
+    uint64_t crc = 0; 
+    ITERATE_SDR_BLOCKS(i,
+        crc = crc64(crc, &(name.blocks[i]), sizeof(SDR_BLOCK_TYPE));
+    )
+    concept->name_hash = crc;
 }
