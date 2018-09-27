2cae14bcda5ac46554803e58d9b8611fb0555825
Patrick Hammer <patham9@gmail.com>
2018-09-27 04:07:44 +0100

    inference rules finished

--- src/Inference.c
+++ src/Inference.c
@@ -79,7 +79,38 @@ TruthValue intersection(TruthValue v1, TruthValue v2)
     return getTruthValue(f, c);
 }
 
-// {Event task a., Postcondition belief <a =/> b>.} |- Derived event task b.
+//{Event task a., Event belief b.} |- Derived event task (&/,a,b).
+Task Inference_BeliefEventIntersection(Task *a, Task *b)
+{
+    TruthValue conclusionTruth = intersection(a->truth, b->truth);
+    SDR sdr = SDR_Tuple(&a->sdr,&b->sdr);
+    Stamp stamp = {0};
+    Task dummy = { .sdr = sdr, .type = JUDGMENT, .truth = conclusionTruth, .stamp = stamp, .priority = 0 };
+    return dummy;
+}
+
+//{Event task a., Event belief b.} |- Precondition and Postcondition belief <a =/> c>.
+Task Inference_BeliefInduction(Task *subject, Task *predicate)
+{
+    TruthValue conclusionTruth = induction(subject->truth, predicate->truth);
+    SDR sdr = SDR_Tuple(&subject->sdr, &predicate->sdr);
+    Stamp stamp = {0};
+    Task dummy = { .sdr = sdr, .type = JUDGMENT, .truth = conclusionTruth, .stamp = stamp, .priority = 0 };
+    return dummy;
+}
+
+//{Precondition or Postcondition belief a., Precondition or Postcondition belief a.} |- 
+// Precondition or Postcondition belief a.
+Task Inference_BeliefRevision(Task *a, Task *b)
+{
+    TruthValue conclusionTruth = revision(a->truth, b->truth);
+    SDR sdr = compound->sdr;
+    Stamp stamp = {0};
+    Task dummy = { .sdr = sdr, .type = JUDGMENT, .truth = conclusionTruth, .stamp = stamp, .priority = 0 };
+    return dummy;
+}
+
+//{Event task a., Postcondition belief <a =/> b>.} |- Derived event task b.
 Task Inference_BeliefEventDeduction(Task *component, Task *compound)
 {
     TruthValue conclusionTruth = deduction(compound->truth, component->truth);
@@ -89,19 +120,32 @@ Task Inference_BeliefEventDeduction(Task *component, Task *compound)
     return dummy;
 }
 
-/*
-//{Event task a!, Precondition belief <a =/> b>.} |- Derived event task b!
-Task inference_eventAbduction(SDR *compound, TruthValue compoundTruth, SDR *component, TruthValue componentTruth, TruthValue *conclusionTruth) {
-    *conclusionTruth = abduction(compoundTruth, componentTruth);
-    return inference_detachment_forward(compound, component);
-}
-*/
-
-//{Event task a., Event belief b.} |- Precondition and Postcondition belief <a =/> c>.
-Task Inference_eventInduction(Task *subject, Task *predicate) {
-    TruthValue conclusionTruth = induction(subject->truth, predicate->truth);
-    SDR sdr = SDR_Tuple(&subject->sdr, &predicate->sdr);
+//{Event task b!, Postcondition belief <a =/> b>.} |- Derived event task a!
+Task Inference_GoalEventDeduction(Task *component, Task *compound)
+{
+    TruthValue conclusionTruth = deduction(compound->truth, component->truth);
+    SDR sdr = SDR_TupleGetFirstElement(&compound->sdr,&component->sdr);
     Stamp stamp = {0};
     Task dummy = { .sdr = sdr, .type = JUDGMENT, .truth = conclusionTruth, .stamp = stamp, .priority = 0 };
+    return dummy;
+}
+
+//{Event task b., Postcondition belief <a =/> b>.} |- Derived event task a.
+Task Inference_BeliefEventAbduction(Task *component, Task *compound)
+{
+    TruthValue conclusionTruth = abduction(compound->truth, component->truth);
+    SDR sdr = SDR_TupleGetFirstElement(&compound->sdr,&component->sdr);
+    Stamp stamp = {0};
+    Task dummy = { .sdr = sdr, .type = JUDGMENT, .truth = conclusionTruth, .stamp = stamp, .priority = 0 };
+    return dummy;
+}
+
+//{Event task a!, Precondition belief <a =/> b>.} |- Derived event task b!
+Task Inference_GoalEventAbduction(Task *component, Task *compound)
+{
+    TruthValue conclusionTruth = abduction(compound->truth, component->truth);
+    SDR sdr = SDR_TupleGetSecondElement(&compound->sdr,&component->sdr);
+    Stamp stamp = {0};
+    Task dummy = { .sdr = sdr, .type = GOAL, .truth = conclusionTruth, .stamp = stamp, .priority = 0 };
     return dummy;
 }
--- src/Inference.h
+++ src/Inference.h
@@ -20,34 +20,22 @@
 //----------//
 #define EVIDENTAL_HORIZON 1.0
 
-
 //Methods//
 //-------//
-//Get a stack-allocated truth valie
-TruthValue getTruthValue(double frequency, double confidence);
-//The output is conjunctively determined by the inputs
-double and(double a, double b);
-//The output is disjunctively determined by the input
-double or(double a, double b);
+//{Event task a., Event belief b.} |- Derived event task (&/,a,b).
+Task Inference_BeliefEventIntersection(Task *a, Task *b);
+//{Event task a., Event belief b.} |- Precondition and Postcondition belief <a =/> c>.
+Task Inference_BeliefInduction(Task *subject, Task *predicate);
 //{Precondition or Postcondition belief a., Precondition or Postcondition belief a.} |- 
 // Precondition or Postcondition belief a.
-TruthValue revision(TruthValue v1, TruthValue v2);
-//{Event task a., Postcondition belief <a =/> b>.} |- Derived event task b.
-//{Event task b!, Precondition belief <a =/> b>.} |- Derived event task a!
-TruthValue deduction(TruthValue v1, TruthValue v2);
-//{Event task a., Event belief b.} |- Precondition and Postcondition belief <a =/> c>.
-TruthValue induction(TruthValue v1, TruthValue v2);
-//{Event task b., Precondition belief <a =/> b>.} |- Derived event task a.
-//{Event task a!, Precondition belief <a =/> b>.} |- Derived event task b!
-TruthValue abduction(TruthValue v1, TruthValue v2);
-//{Event task a., Event belief b.} |- Derived event task (&/,a,b).
-TruthValue intersection(TruthValue v1, TruthValue v2);
-
-
+Task Inference_BeliefRevision(Task *a, Task *b);
 //{Event task a., Postcondition belief <a =/> b>.} |- Derived event task b.
 Task Inference_BeliefEventDeduction(Task *component, Task *compound);
-
-//{Event task a., Event belief b.} |- Precondition and Postcondition belief <a =/> c>.
-Task Inference_eventInduction(Task *subject, Task *predicate);
+//{Event task b!, Postcondition belief <a =/> b>.} |- Derived event task a!
+Task Inference_GoalEventDeduction(Task *component, Task *compound);
+//{Event task b., Postcondition belief <a =/> b>.} |- Derived event task a.
+Task Inference_BeliefEventAbduction(Task *component, Task *compound);
+//{Event task a!, Precondition belief <a =/> b>.} |- Derived event task b!
+Task Inference_GoalEventAbduction(Task *component, Task *compound);
 
 #endif
