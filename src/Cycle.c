be8b3a79873dbe423710f72dede51dab206edb95
Patrick Hammer <patham9@gmail.com>
2018-10-09 23:12:20 +0000

    more of cycle

--- src/Cycle.c
+++ src/Cycle.c
@@ -10,9 +10,11 @@ void cycle()
         Concept *c = Memory_getClosestConcept(&e);
         FIFO *fifo =  e->type == EVENT_TYPE_BELIEF ? &c->event_beliefs : &c->event_goals;
         FIFO_AddAndRevise(e, fifo);
-       // e->attention = Attention_forgetTask(&e->attention);
-       // PriorityQueue_Push(&events, e->
-        //todo there is more that needs to happen
+        e->attention = Attention_forgetTask(&e->attention);
+        PriorityQueue_Push_Feedback pushed = PriorityQueue_Push(&events, e->attention.priority);
+        Event *cpushed = pushed.addedItem.address;
+        *cpushed = *e;
+        //todo there is more that needs to happen for event-concept interaction, inference! :)
     }
     for(int i=0; i<concepts.itemsAmount; i++) //as all concepts are forgotten the order won't change
     { //making this operation very cheap, not demanding any heap operation
