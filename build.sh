7a8c5f56f4eaf79fee29bbfa706872190c389532
Patrick Hammer <patham9@gmail.com>
2019-06-09 13:49:39 +0000

    Update: build.sh: improved build script as Seth suggested

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
-Str=""
-for i in ./src/*.c
-do
-    Str=$Str" ${i%.c}.c"
-done
-echo $Str
+Str=`ls src/*.c | xargs`
+echo $Str 
 gcc -DSDR_BLOCK_TYPE=__uint128_t -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 -Wall -Werror $Str -lm -oANSNA
