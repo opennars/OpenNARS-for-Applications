b9d43c4ab7e7829fd9725038263bdec8167ab642
Patrick Hammer <patham9@gmail.com>
2019-07-24 11:44:49 +0000

    kick out format security warning

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 Str=`ls src/*.c | xargs`
 echo $Str 
-gcc -DSDR_BLOCK_TYPE=__uint128_t -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 -Wall -Werror $Str -lm -oANSNA
+gcc -DSDR_BLOCK_TYPE=__uint128_t -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 -Wall -Werror -Wno-format-security $Str -lm -oANSNA
