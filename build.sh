b80969f3fdb6a6109f65ab22119f4ea52b2f88ef
Patrick Hammer <patham9@gmail.com>
2019-12-27 18:53:07 +0000

    Update: build.sh: String for compiler flags not necessary.

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 Str=`ls src/*.c | xargs`
 echo $Str
 echo "Compilation started: Unused code will be printed and removed from the binary:"
-compileparams="-ffunction-sections -fdata-sections -Wl,--gc-sections -Wl,--print-gc-sections -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 -Wall -Wextra -Wformat-security $Str -lm -oYAN"
-gcc $compileparams
+gcc -ffunction-sections -fdata-sections -Wl,--gc-sections -Wl,--print-gc-sections -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 -Wall -Wextra -Wformat-security $Str -lm -oYAN
 ./YAN NAL_GenerateRuleTable > ./src/RuleTable.c
 echo "Done."
 echo "Compile again on first compilation, or if changes to rule files have been made as well!"
