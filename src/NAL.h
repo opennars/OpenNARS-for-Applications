e8b0a557dfa257e410452c688f99393013ebe926
Patrick Hammer <patham9@gmail.com>
2020-01-22 10:29:04 +0000

    Update: Removal of what will once be backward rules, another story added.

--- src/NAL.h
+++ src/NAL.h
@@ -35,14 +35,11 @@ void NAL_DerivedEvent(Term conclusionTerm, long conclusionOccurrence, Truth conc
 #ifdef H_NAL_RULES
 
 //NAL1 rules
-R1( (S --> P), |-, (P --> S), Truth_Conversion )
 R2( (S --> M), (M --> P), |-, (S --> P), Truth_Deduction )
 R2( (A --> B), (A --> C), |-, (C --> B), Truth_Abduction )
 R2( (A --> C), (B --> C), |-, (B --> A), Truth_Induction )
 R2( (A --> B), (B --> C), |-, (C --> A), Truth_Exemplification )
 //NAL2 rules
-R1( (S <-> P), |-, (P <-> S), Truth_StructuralDeduction )
-R2( (S --> P), (P --> S), |-, (S <-> P), Truth_Intersection )
 R2( (P --> M), (S --> M), |-, (S <-> P), Truth_Comparison )
 R2( (M --> P), (M --> S), |-, (S <-> P), Truth_Comparison )
 R2( (M --> P), (S <-> M), |-, (S --> P), Truth_Analogy )
@@ -50,8 +47,6 @@ R2( (P --> M), (S <-> M), |-, (P --> S), Truth_Analogy )
 R2( (M <-> P), (S <-> M), |-, (S <-> P), Truth_Resemblance )
 R1( ({A} <-> {B}), |-, (A <-> B), Truth_StructuralDeduction )
 R1( ([A] <-> [B]), |-, (A <-> B), Truth_StructuralDeduction )
-R1( (S <-> P), |-, (S --> P), Truth_StructuralDeduction )
-R1( (S --> P), |-, (S <-> P), Truth_StructuralAbduction )
 //NAL3 rules
 R1( ((S | P) --> M), |-, (S --> M), Truth_StructuralDeduction )
 R1( (M --> (S & P)), |-, (S --> M), Truth_StructuralDeduction )
