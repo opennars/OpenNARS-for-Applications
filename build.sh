fc0435eb8a58bfc2fe4119a8dba15a92468c55cb
Patrick Hammer <patham9@gmail.com>
2020-02-06 05:20:23 +0000

    Update: Allow passing of argument to build.sh for profiling etc.

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ BaseFlags="-D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 $Str -lm -oYAN"
 gcc -DSTAGE=1 -Wall -Wextra -Wformat-security $BaseFlags
 echo "First stage done, generating RuleTable.c now, and finishing compilation."
 ./YAN NAL_GenerateRuleTable > ./src/RuleTable.c
-gcc -ffunction-sections -fdata-sections -Wl,--gc-sections -Wl,--print-gc-sections -DSTAGE=2 $BaseFlags src/RuleTable.c
+gcc $1 -ffunction-sections -fdata-sections -Wl,--gc-sections -Wl,--print-gc-sections -DSTAGE=2 $BaseFlags src/RuleTable.c
 echo "Done."
