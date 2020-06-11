b9b286baa108835f9c758cafc8aa5f69da53026d
Patrick Hammer <patham9@gmail.com>
2020-06-11 05:31:26 +0000

    Update: Decision.c: allow goal evidence to accumulate in ConsiderImplication

--- src/Decision.c
+++ src/Decision.c
@@ -86,30 +86,35 @@ static Decision Decision_ConsiderImplication(long currentTime, Event *goal, int 
     if(precondition != NULL)
     {
         Event ContextualOperation = Inference_GoalDeduction(goal, imp); //(&/,a,op())! :\:
-        double operationGoalTruthExpectation = Truth_Expectation(Inference_OperationDeduction(&ContextualOperation, precondition, currentTime).truth); //op()! :|:
-        IN_DEBUG
-        (
-            printf("CONSIDERED PRECON: desire=%f ", operationGoalTruthExpectation);
-            Narsese_PrintTerm(&prec->term);
-            fputs("\nCONSIDERED PRECON truth ", stdout);
-            Truth_Print(&precondition->truth);
-            fputs("CONSIDERED goal truth ", stdout);
-            Truth_Print(&goal->truth);
-            fputs("CONSIDERED imp truth ", stdout);
-            Truth_Print(&imp->truth);
-            printf("CONSIDERED time %ld\n", precondition->occurrenceTime);
-            Narsese_PrintTerm(&precondition->term); puts("");
-        )
+        Event OpEvent = Inference_OperationDeduction(&ContextualOperation, precondition, currentTime);
         //<(precon &/ <args --> ^op>) =/> postcon>. -> [$ , postcon precon : _ _ _ _ args ^op
-        Term operation = Term_ExtractSubterm(&imp->term, 4); //^op or [: args ^op]
-        if(!Narsese_isOperator(operation.atoms[0])) //it is an operation with args, not just an atomic operator, so remember the args
+        OpEvent.term = Term_ExtractSubterm(&imp->term, 4); //^op or [: args ^op]
+        Concept *opc = Memory_Conceptualize(&OpEvent.term, currentTime);
+        if(opc != NULL)
         {
-            Narsese_PrintTerm(&imp->term); puts("wtf");
-            assert(Narsese_isOperator(operation.atoms[2]), "If it's not atomic, it needs to be an operation with args here");
-            decision.arguments = Term_ExtractSubterm(&imp->term, 9); 
+            opc->goal_spike = Inference_RevisionAndChoice(&opc->goal_spike, &OpEvent, currentTime, NULL);
+            double operationGoalTruthExpectation = Truth_Expectation(opc->goal_spike.truth); //op()! :|:
+            IN_DEBUG
+            (
+                printf("CONSIDERED PRECON: desire=%f ", operationGoalTruthExpectation);
+                Narsese_PrintTerm(&prec->term);
+                fputs("\nCONSIDERED PRECON truth ", stdout);
+                Truth_Print(&precondition->truth);
+                fputs("CONSIDERED goal truth ", stdout);
+                Truth_Print(&goal->truth);
+                fputs("CONSIDERED imp truth ", stdout);
+                Truth_Print(&imp->truth);
+                printf("CONSIDERED time %ld\n", precondition->occurrenceTime);
+                Narsese_PrintTerm(&precondition->term); puts("");
+            )
+            if(!Narsese_isOperator(OpEvent.term.atoms[0])) //it is an operation with args, not just an atomic operator, so remember the args
+            {
+                assert(Narsese_isOperator(OpEvent.term.atoms[2]), "If it's not atomic, it needs to be an operation with args here");
+                decision.arguments = Term_ExtractSubterm(&imp->term, 9); 
+            }
+            decision.operationID = considered_opi;
+            decision.desire = operationGoalTruthExpectation;
         }
-        decision.operationID = considered_opi;
-        decision.desire = operationGoalTruthExpectation;
     }
     return decision;
 }
