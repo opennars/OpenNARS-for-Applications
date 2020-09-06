e164a54591449817ffbcdb9861dcb3de539a7ea6
Patrick Hammer <patham9@gmail.com>
2020-09-06 05:25:10 +0000

    Update: set sse flags by default, this will also make float behavior consistent among 32/64 bit Intel architecture, and with ARM processors, though for the latter the sse flags need to be removed on compilation

--- libbuild.sh
+++ libbuild.sh
@@ -8,12 +8,11 @@ sudo rm /usr/local/lib/libONA.so
 Str=`ls src/*.c src/NetworkNAR/*.c | xargs`
 echo $Str
 echo "Compilation started:"
-BaseFlags="-pthread -lpthread -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 $Str -lm"
+BaseFlags="-mfpmath=sse -msse2 -pthread -lpthread -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 $Str -lm"
 gcc -DSTAGE=1 -Wall -Wextra -Wformat-security $BaseFlags -oNAR_first_stage
 echo "First stage done, generating RuleTable.c now, and finishing compilation."
 mv src/main.c src/main_
 Str=`ls src/*.c src/NetworkNAR/*.c | xargs`
-BaseFlags="-pthread -lpthread -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 $Str -lm"
 ./NAR_first_stage NAL_GenerateRuleTable > ./src/RuleTable.c
 gcc -c -DSTAGE=2 $BaseFlags src/RuleTable.c
 ar rcs libONA.a *.o
