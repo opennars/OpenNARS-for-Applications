7b89049bda506453749e2a811c493dd6974db488
Patrick Hammer <patham9@gmail.com>
2020-09-09 18:05:22 +0000

    Update: libbuild.sh: fixed without introducing BaseFlags twice

--- libbuild.sh
+++ libbuild.sh
@@ -8,13 +8,13 @@ sudo rm /usr/local/lib/libONA.so
 Str=`ls src/*.c src/NetworkNAR/*.c | xargs`
 echo $Str
 echo "Compilation started:"
-BaseFlags="-mfpmath=sse -msse2 -pthread -lpthread -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 $Str -lm"
-gcc -DSTAGE=1 -Wall -Wextra -Wformat-security $BaseFlags -oNAR_first_stage
+BaseFlags="-mfpmath=sse -msse2 -pthread -lpthread -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -lm"
+gcc -DSTAGE=1 -Wall -Wextra -Wformat-security $BaseFlags $Str -oNAR_first_stage
 echo "First stage done, generating RuleTable.c now, and finishing compilation."
 mv src/main.c src/main_
 Str=`ls src/*.c src/NetworkNAR/*.c | xargs`
 ./NAR_first_stage NAL_GenerateRuleTable > ./src/RuleTable.c
-gcc -c -DSTAGE=2 $BaseFlags src/RuleTable.c
+gcc -c -DSTAGE=2 $BaseFlags $Str src/RuleTable.c
 ar rcs libONA.a *.o
 rm -rf *.o NAR_first_stage
 gcc -c -fPIC -DSTAGE=2 $BaseFlags src/RuleTable.c
