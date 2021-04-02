170ab05950d276e3750009cb6a25b12c74283508
Patrick Hammer <patham9@gmail.com>
2021-04-02 00:43:57 +0000

    Update: don't skip last concept to export

--- concept_usefulness_filter.py
+++ concept_usefulness_filter.py
@@ -27,7 +27,7 @@ import sys
 lines = []
 curline = None
 for line in sys.stdin:
-    if line.startswith("//*"):
+    if line.startswith("//*done"):
         continue
     if line.startswith("*"):
         print(line, end='')
@@ -42,4 +42,4 @@ lines = [line for line in lines if line.strip() != "" and ": { \"priority\":" in
 lines.sort(key=lambda line: -float(line.split("\"usefulness\": ")[1].split(",")[0]))
 displayN = len(lines) if len(sys.argv) <= 1 else int(sys.argv[1])
 for (i,x) in enumerate(lines[0:displayN]):
-    print("//{i="+str(i)+"}", x, end='')
+    print("//{i="+str(i)+"}", x, end='', flush=True)
