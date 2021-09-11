706fef85c179ad5d807a3c25b9162259f0ca104c
Patrick Hammer <patham9@gmail.com>
2021-09-11 22:07:19 +0200

    Update: don't change build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 Str=`ls src/*.c src/NetworkNAR/*.c | xargs`
 echo $Str
 echo "Compilation started:"
-BaseFlags=" -pthread -lpthread -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 $Str -lm -oNAR"
+BaseFlags="-mfpmath=sse -msse2 -pthread -lpthread -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 $Str -lm -oNAR"
 gcc -DSTAGE=1 -Wall -Wextra -Wformat-security $BaseFlags
 echo "First stage done, generating RuleTable.c now, and finishing compilation."
 ./NAR NAL_GenerateRuleTable > ./src/RuleTable.c
