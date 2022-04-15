4a687bb0fdc0bc5e07b8d253a2f21c4046ead8d2
Adrian Borucki <ab@synthillect.ai>
2022-04-15 15:16:25 +0200

    Use SSE2 only on x86/x64

--- build.sh
+++ build.sh
@@ -7,11 +7,18 @@ fi
 
 rm NAR
 rm src/RuleTable.c
-Str=`ls src/*.c src/NetworkNAR/*.c | xargs`
-echo $Str
+sources=`ls src/*.c src/NetworkNAR/*.c | xargs`
+echo $sources
 echo "Compilation started:"
-BaseFlags="-g -pthread -lpthread -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 $Str -lm -oNAR"
+BaseFlags="-g -pthread -lpthread -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 $sources -lm -oNAR"
 NoWarn="-Wno-tautological-compare -Wno-dollar-in-identifier-extension -Wno-unused-parameter -Wno-unused-variable"
+
+# if CPU architecture is x86/x64, then use SSE2
+if [[ "$(uname -m)" == "x86"* ]]; then
+    echo "Compiling for x86/x64."
+    BaseFlags="-mfpmath=sse -msse2 $BaseFlags"
+fi
+
 $CC -DSTAGE=1 -Wall -Wextra -Wformat-security $NoWarn $BaseFlags
 echo "First stage done, generating RuleTable.c now, and finishing compilation."
 ./NAR NAL_GenerateRuleTable > ./src/RuleTable.c
