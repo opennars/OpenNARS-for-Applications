cfd3bb90614a9b7f2b269a4a7342857c59c22b48
Adrian Borucki <ab@synthillect.ai>
2022-05-12 07:27:45 +0200

    Update: build.sh: fixed a problem with  being in the non-SSE invocation at the end instead of

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ echo $Str
 echo "Compilation started:"
 BaseFlags="-g -pthread -lpthread -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 $Str -lm -oNAR"
 NoWarn="-Wno-tautological-compare -Wno-dollar-in-identifier-extension -Wno-unused-parameter -Wno-unused-variable"
-gcc -DSTAGE=1 -Wall -Wextra -Wformat-security $NoWarn $BaseFlags
+gcc $@ -DSTAGE=1 -Wall -Wextra -Wformat-security $NoWarn $BaseFlags
 echo "First stage done, generating RuleTable.c now, and finishing compilation."
 ./NAR NAL_GenerateRuleTable > ./src/RuleTable.c
-gcc $@ -mfpmath=sse -msse2 -DSTAGE=2 $NoWarn $BaseFlags src/RuleTable.c || (echo "Error with SSE, hence compiling without SSE:" && gcc $1 -DSTAGE=2 $NoWarn $BaseFlags src/RuleTable.c)
+gcc $@ -mfpmath=sse -msse2 -DSTAGE=2 $NoWarn $BaseFlags src/RuleTable.c || (echo "Error with SSE, hence compiling without SSE:" && gcc $@ -DSTAGE=2 $NoWarn $BaseFlags src/RuleTable.c)
 echo "Done."
