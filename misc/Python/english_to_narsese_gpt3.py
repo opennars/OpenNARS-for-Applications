d8314f7dd411499ab2746e8679c60bdc507dc348
Patrick Hammer <patham9@gmail.com>
2023-04-14 15:20:43 +0200

    Update: english_to_narsese_gpt3.py: some needed postprocessing to make it practical

--- misc/Python/english_to_narsese_gpt3.py
+++ misc/Python/english_to_narsese_gpt3.py
@@ -29,44 +29,63 @@ from nltk.corpus import wordnet
 import openai
 
 openai.api_key = "YOUR_KEY"
-eternal = True #whether to use event or eternal output
+Eternal = True #whether to use event or eternal output
+Negation = False #whether negated statements should also be generated
 PrintInput=False
 Prepositions = False
 CleanRelations = True
 levenstein_threshold = 0.8
 wup_threshold = 0.8
-gpt_prompt = """
-def PosRelation(noun,verb,noun): ... #put true relation into database
-def PosRelation(noun,"IsA",noun): ... #put true category into database
-def PosProperty(noun,adjective): ... #put true property into database
-def NegRelation(noun,verb,noun): ... #put false relation into database
-def NegRelation(noun,"IsA",noun): ... #put false category into database
-def NegProperty(noun,adjective): ... #put false property into database
+if Negation:
+    gpt_prompt = """
+PosRelation(noun,verb,noun)
+PosRelation(noun,"IsA",noun)
+PosProperty(noun,adjective)
+NegRelation(noun,verb,noun)
+NegRelation(noun,"IsA",noun)
+NegProperty(noun,adjective)
+Capture the complete sentence meaning with code that calls the four functions, and only use a single word per argument.
+The sentence: 
+"""
+else:
+    gpt_prompt = """
+Relation(noun,verb,noun)
+Relation(noun,"IsA",noun)
+Property(noun,adjective)
 Capture the complete sentence meaning with code that calls the two functions, and only use a single word per argument.
 The sentence: 
 """
 
 if "EventOutput" in sys.argv:
-    eternal = False
+    Eternal = False
 
 if "Prepositions" in sys.argv:
     Prepositions = True
 
+questionwords = ["what", "where","which", "who", "when"]
 used_verbs = set([])
 lemma = WordNetLemmatizer()
 def Lemmatize(word, tag, isQuestion, s=None, p=None):
     global used_verbs
     ret = lemma.lemmatize(word.lower(), pos = tag).strip().lower().replace(" ","_")
     #print("//!!!",ret)
-    varwords = ["something","someone", "somewhere", "somewhen", "somewhat", "nowhere", "what", "where","which", "who", "when", "noun", "none", "unknown", "unspecified"]
+    varwords = ["something","someone", "somewhere", "somewhen", "somewhat", "nowhere"] + questionwords + ["noun", "none", "unknown", "unspecified", "it", "they", "he", "she", "it"]
     if isQuestion:
         for word in varwords:
-            ret = ret.replace(word, "?1")
+            if word == ret:
+                ret = "?1"
+    else:
+        for word in varwords:
+            if word == ret:
+                return "", "", ""
     if ret in used_verbs: #if already a used verb, just use it
         return ret, s, p
+    prepositions = ["to", "on", "in", "onto", "of", "over", "from"]
+    if tag == wordnet.NOUN and ret in prepositions:
+        return "", "", ""
     if tag == wordnet.VERB: #else we search for a highly overlapping one and use that one
         #we get rid of prepositions next:
-        if ret == "isa" or ret == "is_a" or ret == "be" or ret == "are":
+        if ret == "is" or ret == "isa" or ret == "is_a" or ret == "be" or ret == "are" or ret == "were":
             return "IsA", s, p
         if not Prepositions:
             prepositions_inversing = ["by"]
@@ -79,11 +98,15 @@ def Lemmatize(word, tag, isQuestion, s=None, p=None):
                     ret = ret[:-(len(word)+1)]
                     print(f"//Args switched for relation '{ret}' due to preposition '{word}'")
                     break
-            for word in ["to", "on", "in", "onto", "of", "over", "from"]:
+            for word in prepositions:
                 if "is_" + word == ret or "is" + word == ret:
                     ret = word
                 elif ret.endswith("_"+word):
                     ret = ret[:-(len(word)+1)]
+                    if ret == "are" or ret == "is":
+                        ret = word
+                elif ret.startswith(word+"_"):
+                    ret = ret[(len(word)+1):]
                     if ret == "are" or ret == "is":
                         ret = word
         if CleanRelations:
@@ -114,10 +137,16 @@ def Lemmatize(word, tag, isQuestion, s=None, p=None):
         #replace verb with simpler component
         for x in used_verbs:
             if x in ret:
-                if x != ret:
+                if x != ret and x not in prepositions:
                     print(f"//using {x} instead of {ret} since the former is simpler")
                     return x, s, p
+        ret = lemma.lemmatize(ret.lower(), pos = tag).strip().lower().replace(" ","_")
+        if ret == "location" or ret == "locate":
+            ret = "in"
         used_verbs.add(ret) #and if there is no such, use the new verb
+    hasNoAlnum = True in [x.isalnum() for x in ret]
+    if not hasNoAlnum and isQuestion or ret == "bubub":
+        ret = "?1"
     return ret, s, p
 
 def Relation(s, v, p, punctuation_tv, isQuestion):
@@ -140,21 +169,43 @@ def Property(s, p, punctuation_tv, isQuestion):
 
 def process_commands(commands, isQuestion):
     for x in commands:
-        if (x.startswith("PosProperty(") or x.startswith("PosRelation(") or x.startswith("NegProperty(") or x.startswith("NegRelation(")) and x.endswith(")"):
+        Positive = True
+        truth = "{1.0 0.9}"
+        if x.startswith("Pos"):
+            x = x[3:]
+        if x.startswith("Neg"):
+            x = x[3:]
+            Positive = False
+            truth = "{0.0 0.9}"
+        if (x.startswith("Property(") or x.startswith("Relation(")) and x.endswith(")"):
             #print("//",x)
             s_v_p = x.split("(")[1].split(")")[0].replace("\"","").replace("'","").split(",")
-            truth = "{1.0 0.9}"
-            if x.startswith("Neg"):
-                truth = "{0.0 0.9}"
-            x = x[3:] #cut off Neg/Pos
             if len(s_v_p) > 3:
                 continue
-            eventMarker = "" if eternal else " :|:"
+            eventMarker = "" if Eternal else " :|:"
             punctuation_tv = f"?{eventMarker}" if isQuestion else f".{eventMarker} {truth}"
             if x.startswith("Property"):
                 Property(*s_v_p, punctuation_tv, isQuestion)
             if x.startswith("Relation"):
                 Relation(*s_v_p, punctuation_tv, isQuestion)
+
+def process_input(inp):
+    isQuestion = inp.endswith("?")
+    if inp.endswith("?") or inp.endswith("."):
+        inp = inp[0:-1]
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+          messages=[
+            {"role": "user", "content": gpt_prompt + inp}],
+        max_tokens=100,
+        temperature=0,
+    )
+    commands = response['choices'][0]['message']['content'].split("\n")
+    process_commands(commands, isQuestion)
+    for arg in sys.argv:
+        if arg.startswith("BetweenEventDelay="):
+            delay = arg.split("BetweenEventDelay=")[1]
+            print(delay)
 
 while True:
     try:
@@ -163,6 +214,7 @@ while True:
             print("//Sentence input:", inp)
     except:
         exit(0)
+    #process commands
     if len(inp) == 0:
         print("\n")
     elif inp.startswith("*eternal=false"):
@@ -171,20 +223,15 @@ while True:
         eternal = True
     elif inp.isdigit() or inp.startswith("*") or inp.startswith("(") or inp.startswith("<"):
         print(inp)
-    else:
-        isQuestion = inp.endswith("?")
-        if inp.endswith("?") or inp.endswith("."):
-            inp = inp[0:-1]
-        response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-              messages=[
-                {"role": "user", "content": gpt_prompt + inp}],
-            max_tokens=100,
-            temperature=0,
-        )
-        commands = response['choices'][0]['message']['content'].split("\n")
-        process_commands(commands, isQuestion)
-        for arg in sys.argv:
-            if arg.startswith("BetweenEventDelay="):
-                delay = arg.split("BetweenEventDelay=")[1]
-                print(delay)
+    else: #process inputs
+        process_input(inp)
+        #gpt has issues with qu var, so let's use a dummy noun:
+        if inp.endswith("?"):
+            if inp.startswith("where"):
+                print("//Additional question pass for: where")
+                process_input("in bubub " + inp[:-1] + " located?")
+            else:
+                for word in questionwords:
+                    if inp.startswith(word):
+                        print("//Additional question pass for:", word)
+                        process_input(inp.replace(word + " ", "bubub "))
