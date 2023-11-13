cfddd29f2a864eb3bb447b7461a68f6950d0190e
Patrick Hammer <patham9@gmail.com>
2023-11-13 12:05:59 +0100

    Update: stack size needs to be greater to not cause issues with emscripten

--- build_browser.sh
+++ build_browser.sh
@@ -13,7 +13,5 @@ echo "First stage done, generating RuleTable.c now, and finishing compilation."
 export WASI_SDK_PATH=/home/tc/wasi-sdk
 CC="${WASI_SDK_PATH}/bin/clang --sysroot=${WASI_SDK_PATH}/share/wasi-sysroot"
 BaseFlags="-g -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 $Str -lm -oNAR.html"
-emcc $@ -DBROWSER -DSTAGE=2 $NoWarn $BaseFlags -s TOTAL_MEMORY=900mb src/RuleTable.c
+emcc $@ -DBROWSER -sSTACK_SIZE=10485760 -DSTAGE=2 $NoWarn $BaseFlags -s TOTAL_MEMORY=900mb src/RuleTable.c
 echo "Done."
-
-#-sEXPORTED_FUNCTIONS=_main,_NAR_INIT,_NAR_AddInput -sEXPORTED_RUNTIME_METHODS=ccall,cwrap
