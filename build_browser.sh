e5e2f3b555154f0bd225e752341c5049948ebc47
Patrick Hammer <patham9@gmail.com>
2023-11-15 13:06:53 +0100

    Update: also main exported

--- build_browser.sh
+++ build_browser.sh
@@ -13,5 +13,5 @@ echo "First stage done, generating RuleTable.c now, and finishing compilation."
 export WASI_SDK_PATH=/home/tc/wasi-sdk
 CC="${WASI_SDK_PATH}/bin/clang --sysroot=${WASI_SDK_PATH}/share/wasi-sysroot"
 BaseFlags="-g -D_POSIX_C_SOURCE=199506L -pedantic -std=c99 -g3 -O3 $Str -lm -oNAR.html"
-emcc $@ -sEXPORTED_FUNCTIONS=_Shell_NARInit,_Shell_ProcessInput,_free -sEXPORTED_RUNTIME_METHODS=stringToNewUTF8 -sSTACK_SIZE=10485760 -DSTAGE=2 $NoWarn $BaseFlags -s TOTAL_MEMORY=900mb src/RuleTable.c
+emcc $@ -sEXPORTED_FUNCTIONS=_main,_Shell_NARInit,_Shell_ProcessInput,_free -sEXPORTED_RUNTIME_METHODS=stringToNewUTF8 -sSTACK_SIZE=10485760 -DSTAGE=2 $NoWarn $BaseFlags -s TOTAL_MEMORY=900mb src/RuleTable.c
 echo "Done."
